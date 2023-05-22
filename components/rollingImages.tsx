7beed2a826d65c0a9e815085bf2f7245934a7e55
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-05-23 00:33:03 +0100

    replace img with next Image component

--- components/rollingImages.tsx
+++ components/rollingImages.tsx
@@ -1,26 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 export const RollingImages = () => {
   return (
-    <div className="flex items-center justify-center h-56 p-2 my-8 gap-10">
-      <img
-        className="outline outline-8 rounded-3xl bg-slate-200 -rotate-12 p-1"
-        src="/assets/rainbowpuke.png"
-        alt="puke"
-        width={200}
-      />
-      <img
-        className="outline outline-8 rounded-3xl bg-slate-200 -translate-y-6 p-1"
-        src="/assets/pepe.png"
-        alt="pepe"
-        width={200}
-      />
-      <img
-        className="outline outline-8 rounded-3xl bg-slate-200 rotate-12 p-1"
-        src="/assets/icecream.png"
-        alt="icecream"
-        width={200}
-      />
+    <div className="flex items-center justify-center h-fit p-4 my-6 gap-16">
+      <div className="outline outline-2 rounded-xl backdrop-blur-lg -rotate-12 p-1">
+        <Image
+          src="/assets/rainbowpuke.png"
+          alt="puke"
+          height={220}
+          width={200}
+          objectFit="contain"
+        />
+      </div>
+      <div className="outline outline-2 rounded-3xl backdrop-blur-lg -translate-y-6 p-1">
+        <Image src="/assets/pepe.png" alt="pepe" height={200} width={200} />
+      </div>
+      <div className="outline outline-2 rounded-3xl backdrop-blur-lg rotate-12 p-1">
+        <Image
+          src="/assets/icecream.png"
+          alt="icecream"
+          height={200}
+          width={200}
+        />
+      </div>
     </div>
   );
 };
